feb4e5755e3e8495b5c692a065a70e998b8b4fa2
Thodoris <paparrigopoulosthodoris@gmail.com>
2022-06-07 19:44:07 +0300

    add dropdown for the possible options

--- energy-live/src/app/charts/charts.component.ts
+++ energy-live/src/app/charts/charts.component.ts
@@ -7,13 +7,23 @@ import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./charts.component.scss']
 })
 export class ChartsComponent {
+  options = ["Actual Total Load", "Generation Per Type", "Cross-Border Flows"];
+  index = -1;
+  option = "Choose Quantity";
   model !: NgbDateStruct;
   date !: {year: number, month: number};
 
   constructor(private calendar: NgbCalendar) {
+    this.selectToday();
   }
 
   selectToday() {
     this.model = this.calendar.getToday();
+  }
+
+  onChooseOption(idx: number) {
+    this.index = idx;
+    this.option = this.options[idx];
+
   }
 }
