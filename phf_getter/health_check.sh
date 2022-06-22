0f4d67447dad06356208b45f43b8ff0166099abc
Orfeasfil <orfeasfil2000@gmail.com>
2022-06-22 21:47:29 +0300

    package.json files ready and basic files

---
+++ phf_getter/health_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+for kafka_server in "$@";
+do
+	ret=$(kafkacat -L -b $kafka_server  -t maria)
+	printf "%s\n" $ret		
+	while [[ $ret != *"Metadata"* ]];
+	do
+		printf "unsuccesful\n"
+		ret=$(kafkacat -L -b $kafka_server  -t maria)
+		sleep 1	
+	done	
+	printf "succesful for %s\n" $kafka_server
+
+done    	 
+
+exit 0
