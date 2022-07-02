0dc310765701f85589f06ba9328abaa41371aed7
Thodoris <paparrigopoulosthodoris@gmail.com>
2022-07-02 15:08:59 +0300

    fix bug

--- energy-live/src/app/services/data.service.ts
+++ energy-live/src/app/services/data.service.ts
@@ -13,24 +13,24 @@ export class DataService {
 
     dataActualTotalLoad(data: ActualTotalLoad) {
         let params = new HttpParams();
-        params.set('date', data.date.getFullYear() + "-" + data.date.getMonth() + "-" + data.date.getDay());
-        params.set('country', data.country);
+        params = params.set('date', data.date.getFullYear() + "-" + data.date.getMonth() + "-" + data.date.getDay());
+        params = params.set('country', data.country);
 
         return this.httpClient.get<ChartDto>('http://localhost:7080/api/Data', {params}); 
     }
     dataGenerationPerType(data: GenerationPerType) {
         let params = new HttpParams();
-        params.set('date', data.date.getFullYear() + "-" + data.date.getMonth() + "-" + data.date.getDay());
-        params.set('country', data.country);
-        params.set('generationType', data.generationType);
+        params = params.set('date', data.date.getFullYear() + "-" + data.date.getMonth() + "-" + data.date.getDay());
+        params = params.set('country', data.country);
+        params = params.set('generationType', data.generationType);
 
         return this.httpClient.get<ChartDto>('http://localhost:7081/api/Data', {params});         
     }
     dataCrossBoarderFlow(data: CrossBoarderFlow) {
         let params = new HttpParams();
-        params.set('date', data.date.getFullYear() + "-" + data.date.getMonth() + "-" + data.date.getDay());
-        params.set('countryFrom', data.countryFrom);
-        params.set('countryTo', data.countryTo);
+        params = params.set('date', data.date.getFullYear() + "-" + data.date.getMonth() + "-" + data.date.getDay());
+        params = params.set('countryFrom', data.countryFrom);
+        params = params.set('countryTo', data.countryTo);
 
         return this.httpClient.get<ChartDto>('http://localhost:7082/api/Data', {params});  
         
