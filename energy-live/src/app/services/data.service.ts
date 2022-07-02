ed03905f5b4fac7159055ddf93f150733035325d
Thodoris <paparrigopoulosthodoris@gmail.com>
2022-07-02 15:36:57 +0300

    fix api requests

--- energy-live/src/app/services/data.service.ts
+++ energy-live/src/app/services/data.service.ts
@@ -16,7 +16,7 @@ export class DataService {
         params = params.set('date', data.date.getFullYear() + "-" + data.date.getMonth() + "-" + data.date.getDay());
         params = params.set('country', data.country);
 
-        return this.httpClient.get<ChartDto>('http://localhost:7080/api/Data', {params}); 
+        return this.httpClient.get<ChartDto>('http://localhost:7082/api/Data', {params}); 
     }
     dataGenerationPerType(data: GenerationPerType) {
         let params = new HttpParams();
@@ -32,7 +32,7 @@ export class DataService {
         params = params.set('countryFrom', data.countryFrom);
         params = params.set('countryTo', data.countryTo);
 
-        return this.httpClient.get<ChartDto>('http://localhost:7082/api/Data', {params});  
+        return this.httpClient.get<ChartDto>('http://localhost:7083/api/Data', {params});  
         
     }
     // search(search: string, page: number) {
