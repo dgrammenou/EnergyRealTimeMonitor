c4a52f63f6a291007ff36513c229e1a9205e1661
Thodoris <paparrigopoulosthodoris@gmail.com>
2022-07-15 00:14:00 +0300

    desperado

--- agpt_getter/api.js
+++ agpt_getter/api.js
@@ -30,7 +30,7 @@ const pgp = require('pg-promise')({
 pg.types.setTypeParser(1114, str => str);
 
 const db=pgp({
-        host:"paparrigopoulos.com", // host.docker.internal
+        host:"host.docker.internal",
         port:5432,
         user:"postgres",
         password:"Dd2502!..",
